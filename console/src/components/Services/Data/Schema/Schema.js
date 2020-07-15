90f18e794b862624d3a45551c2e75dbfc33921ac
Sameer Kolhar <sameer@hasura.io>
2020-07-15 11:02:27 +0530

    update permissions button

--- console/src/components/Services/Data/Schema/Schema.js
+++ console/src/components/Services/Data/Schema/Schema.js
@@ -44,7 +44,7 @@ import styles from '../../../Common/Common.scss';
 const SchemaPermissionsButton = ({ schema }) => (
   <Link to={getSchemaPermissionsRoute(schema)} style={{ marginLeft: '20px' }}>
     <Button color="white" size="xs" className={styles.add_mar_left_mid}>
-      Show Permissions Summary
+      Take me to Permissions Page
     </Button>
   </Link>
 );
