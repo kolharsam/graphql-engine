922edf24d1ede9bbf2caa2811bb2ea0c2419b553
Aleksandra Sikora <aleksandra@hasura.io>
2020-09-04 19:20:19 +0200

    add fk sql

--- console/src/dataSources/services/postgresql/sqlUtils.ts
+++ console/src/dataSources/services/postgresql/sqlUtils.ts
@@ -1012,8 +1012,8 @@ JOIN (
       AND(r.relkind = ANY (ARRAY ['r'::"char", 'p'::"char"]))) x (tblschema, tblname, colname, cstrname)) constraint_column_usage ON tc.constraint_name::text = constraint_column_usage.constraint_name::text
   AND tc.table_schema::text = constraint_column_usage.table_schema::text
   AND tc.table_name::text = constraint_column_usage.table_name::text
-${generateWhereClause(options, 'tc.table_name', 'tc.table_schema')}
-  AND tc.constraint_type::text = 'PRIMARY KEY'::text
+WHERE tc.constraint_type::text = 'PRIMARY KEY'::text
+  ${generateWhereClause(options, 'tc.table_name', 'tc.table_schema', 'AND')}
 GROUP BY
   tc.table_schema, tc.table_name, tc.constraint_name) as info;
 `;
@@ -1038,8 +1038,13 @@ FROM (
 	FROM
 		information_schema.table_constraints tc
 		JOIN information_schema.key_column_usage kcu USING (constraint_schema, constraint_name)
-    ${generateWhereClause(options, 'tc.table_name', 'tc.constraint_schema')}
-		AND tc.constraint_type::text = 'UNIQUE'::text
+	WHERE tc.constraint_type::text = 'UNIQUE'::text
+    ${generateWhereClause(
+      options,
+      'tc.table_name',
+      'tc.constraint_schema',
+      'AND'
+    )}
 	GROUP BY
 		tc.table_name,
 		tc.constraint_schema,
@@ -1065,7 +1070,61 @@ SELECT n.nspname::text AS table_schema,
    FROM pg_constraint r
      JOIN pg_class ct ON r.conrelid = ct.oid
      JOIN pg_namespace n ON ct.relnamespace = n.oid
-   ${generateWhereClause(options, 'ct.relname', 'n.nspname')}
-   AND r.contype = 'c'::"char"
+   WHERE r.contype = 'c'::"char"
+     ${generateWhereClause(options, 'ct.relname', 'n.nspname', 'AND')}
    ) AS info;
+`;
+
+export const getFKRelations = (options: {
+  schemas: string[];
+  tables: Table[];
+}) => `
+SELECT
+	COALESCE(json_agg(row_to_json(info)), '[]'::JSON)
+FROM (
+	SELECT
+		q.table_schema::text AS table_schema,
+		q.table_name::text AS table_name,
+		q.constraint_name::text AS constraint_name,
+		min(q.constraint_oid)::integer AS constraint_oid,
+		min(q.ref_table_table_schema::text) AS ref_table_table_schema,
+		min(q.ref_table::text) AS ref_table,
+		json_object_agg(ac.attname, afc.attname) AS column_mapping,
+		min(q.confupdtype::text) AS on_update,
+		min(q.confdeltype::text) AS
+		on_delete,
+		json_agg(ac.attname) AS columns,
+		json_agg(afc.attname) AS ref_columns
+	FROM (
+		SELECT
+			ctn.nspname AS table_schema,
+			ct.relname AS table_name,
+			r.conrelid AS table_id,
+			r.conname AS constraint_name,
+			r.oid AS constraint_oid,
+			cftn.nspname AS ref_table_table_schema,
+			cft.relname AS ref_table,
+			r.confrelid AS ref_table_id,
+			r.confupdtype,
+			r.confdeltype,
+			unnest(r.conkey) AS column_id,
+			unnest(r.confkey) AS ref_column_id
+		FROM
+			pg_constraint r
+			JOIN pg_class ct ON r.conrelid = ct.oid
+			JOIN pg_namespace ctn ON ct.relnamespace = ctn.oid
+			JOIN pg_class cft ON r.confrelid = cft.oid
+			JOIN pg_namespace cftn ON cft.relnamespace = cftn.oid
+    WHERE
+      r.contype = 'f'::"char"
+      ${generateWhereClause(options, 'ct.relname', 'ctn.nspname', 'AND')}
+      ) q
+		JOIN pg_attribute ac ON q.column_id = ac.attnum
+			AND q.table_id = ac.attrelid
+		JOIN pg_attribute afc ON q.ref_column_id = afc.attnum
+			AND q.ref_table_id = afc.attrelid
+		GROUP BY
+			q.table_schema,
+			q.table_name,
+			q.constraint_name) AS info;
 `;
