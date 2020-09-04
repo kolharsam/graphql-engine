edc130af8159c5d39b7f518d52f92a1ee62f2557
Sameer Kolhar <kolhar730@gmail.com>
2020-09-04 12:41:46 +0530

    change to object

--- console/src/dataSources/services/mysql/index.tsx
+++ console/src/dataSources/services/mysql/index.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import { DataSourcesAPI } from '../..';
 import { Table } from '../../types';
-import { AlterFKTableInfo, MySQLTrigger, CreatePKArgs } from './types';
-import { getMySQLNameString, escapeText } from './utils';
-import { isColTypeString } from '../postgresql';
-import { isSQLFunction } from '../postgresql/sqlUtils';
+import {
+  getAlterForeignKeySql,
+  getCreateFKeySql,
+  getDropConstraintSql,
+  getRenameTableSql,
+  getDropTriggerSql,
+  getDropColumnSql,
+  getAddColumnSql,
+  getAddUniqueConstraintSql,
+  getDropNotNullSql,
+  getSetCommentSql,
+  getSetColumnDefaultSql,
+  getSetNotNullSql,
+  getAlterColumnTypeSql,
+  getDropColumnDefaultSql,
+  getRenameColumnQuery,
+  checkSchemaModification,
+  getCreateCheckConstraintSql,
+  getCreatePkSql,
+  getCreateTriggerSql,
+  getDropSql,
+} from './sqlUtils';
+
+export const isTable = (table: Table) => {
+  return table.table_name === 'BASE TABLE';
+};
+
+export const displayTableName = (table: Table) => {
+  return isTable(table) ? (
+    <span>{table.table_name}</span>
+  ) : (
+    <i>{table.table_name}</i>
+  );
+};
 
 // Change this to the format to what is present on the postgres side
 export const mysql: DataSourcesAPI = {
-  isTable: (table: Table) => {
-    return table.table_type === 'BASE TABLE';
-  },
-  displayTableName: (table: Table) => {
-    return mysql.isTable(table) ? (
-      <span>{table.table_name}</span>
-    ) : (
-      <i>{table.table_name}</i>
-    );
-  },
+  isTable,
+  displayTableName,
   getFunctionSchema: () => {
     throw new Error('not implemented');
   },
@@ -82,7 +104,7 @@ export const mysql: DataSourcesAPI = {
   cascadeSqlQuery: () => {
     throw new Error('not implemented');
   },
-  dependecyErrorCode: '',
+  dependencyErrorCode: '',
   getCreateTableQueries: () => {
     throw new Error('not implemented');
   },
@@ -102,263 +124,28 @@ export const mysql: DataSourcesAPI = {
   isTimeoutError: () => {
     throw new Error('not implemented');
   },
-  getAlterForeignKeySql: (
-    from: AlterFKTableInfo,
-    to: AlterFKTableInfo,
-    dropConstraint: string,
-    newConstraint: string,
-    onUpdate: string,
-    onDelete: string
-  ) => `
-     alter table ${getMySQLNameString(
-       from.schemaName,
-       from.tableName
-     )} drop foreign key \`${dropConstraint}\`;
-     alter table ${getMySQLNameString(to.schemaName, to.tableName)}
-     add constraint \`${newConstraint}\` foreign key (${from.columns.join(
-    ', '
-  )})
-     references ${getMySQLNameString(to.schemaName, to.tableName)}
-     (${to.columns.join(', ')}) on update ${onUpdate} on delete ${onDelete};
-   `,
-  getCreateFKeySql: (
-    from: AlterFKTableInfo,
-    to: AlterFKTableInfo,
-    newConstraint: string,
-    onUpdate: string,
-    onDelete: string
-  ) => `
-  alter table ${getMySQLNameString(to.schemaName, to.tableName)}
-  add constraint \`${newConstraint}\` foreign key (${from.columns.join(', ')})
-  references ${getMySQLNameString(to.schemaName, to.tableName)}
-  (${to.columns.join(', ')}) on update ${onUpdate} on delete ${onDelete};
-  `,
-  getDropConstraintSql: (
-    tableName: string,
-    schemaName: string,
-    constraintName: string
-  ) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} drop constaint ${constraintName};
-  `,
-  getRenameTableSql: (
-    property = 'table',
-    oldName: string,
-    schemaName: string,
-    newName: string
-  ) => `
-    alter ${property} ${getMySQLNameString(
-    schemaName,
-    oldName
-  )} rename to ${newName};
-  `,
-  getDropTriggerSql: (tableSchema: string, triggerName: string) => `
-    DROP TRIGGER IF EXISTS ${getMySQLNameString(tableSchema, triggerName)};
-  `,
-  getCreateTriggerSql: (
-    tableName: string,
-    tableSchema: string,
-    triggerName: string,
-    trigger: MySQLTrigger
-  ) => `
-    CREATE TRIGGER \`${triggerName}\`
-    ${trigger.action_timing} ${
-    trigger.event_manipulation
-  } ON ${getMySQLNameString(tableSchema, tableName)}
-    FOR EACH ${trigger.action_orientation} ${trigger.action_statement};
-  `,
-  getDropSql: (tableName: string, schemaName: string, property = 'table') => `
-    drop ${property} ${getMySQLNameString(schemaName, tableName)};
-  `,
   getViewDefinitionSql: () => {
     throw new Error('not implemented');
   },
-  getDropColumnSql: (
-    tableName: string,
-    schemaName: string,
-    columnName: string
-  ) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} drop column \`${columnName}\`;
-  `,
-  getAddColumnSql: (
-    tableName: string,
-    schemaName: string,
-    columnName: string,
-    columnType: string,
-    options?: { nullable: boolean; unique: boolean; default: any }
-  ) => {
-    let sql = `alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} add column \`${columnName}\` ${columnType}`;
-
-    if (!options) {
-      return sql;
-    }
-
-    if (options.nullable) {
-      sql += ' null';
-    } else {
-      sql += ' not null';
-    }
-    if (options.unique) {
-      sql += ' unique';
-    }
-    if (options.default) {
-      let defaultVal = options.default;
-      if (isColTypeString(columnType) && !isSQLFunction(options.default)) {
-        defaultVal = `'${options.default}'`;
-      }
-      sql += defaultVal;
-    }
-
-    return sql;
-  },
-  getAddUniqueConstraintSql: (
-    tableName: string,
-    schemaName: string,
-    constraintName: string,
-    columns: string[]
-  ) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} add constraint ${constraintName} unique (${columns.join(', ')});
-  `,
-  getDropNotNullSql: (
-    tableName: string,
-    schemaName: string,
-    columnName: string,
-    columnType?: string
-  ) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} modify \`${columnName}\` ${columnType};
-  `,
-  getSetCommentSql: (
-    on: 'column' | 'table' | string,
-    tableName: string,
-    schemaName: string,
-    columnName: string,
-    comment: string | null,
-    columnType?: string
-  ) => {
-    const commentStr = escapeText(comment);
-
-    if (on === 'column') {
-      return `alter table ${getMySQLNameString(
-        schemaName,
-        tableName
-      )} modify column \`${columnName}\` ${columnType} comment ${commentStr};`;
-    }
-
-    // FIXME: this is only meant to be for on = table
-    return `alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} comment = ${commentStr};`;
-  },
-  getSetColumnDefaultSql: (
-    tableName: string,
-    schemaName: string,
-    columnName: string,
-    defaultValue: any,
-    columnType: string
-  ) => {
-    let defVal = defaultValue;
-    if (isColTypeString(columnType) && !isSQLFunction(defaultValue)) {
-      defVal = `'${defaultValue}'`;
-    }
-    return `
-      alter table ${getMySQLNameString(
-        schemaName,
-        tableName
-      )} alter \`${columnName}\` set default ${defVal};
-    `;
-  },
-  getSetNotNullSql: (
-    tableName: string,
-    schemaName: string,
-    columnName: string,
-    columnType?: string
-  ) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} modify \`${columnName}\` ${columnType} not null;
-  `,
-  getAlterColumnTypeSql: (
-    tableName: string,
-    schemaName: string,
-    columnName: string,
-    columnType: string
-  ) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} modify column \`${columnName}\` ${columnType};  
-  `,
-  getDropColumnDefaultSql: (
-    tableName: string,
-    schemaName: string,
-    columnName: string
-  ) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} alter \`${columnName}\` drop default;
-  `,
-  getRenameColumnQuery: (
-    tableName: string,
-    schemaName: string,
-    newName: string,
-    oldName: string,
-    columnType?: string
-  ) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} change \`${oldName}\` \`${newName}\` ${columnType};
-  `,
   fetchColumnCastsQuery: '',
-  checkSchemaModification: (sql: string) => {
-    const sqlStatements = sql
-      .toLowerCase()
-      .split(';')
-      .map(sqlStr => sqlStr.trim());
-
-    return sqlStatements.some(
-      statement =>
-        statement.startsWith('create ') ||
-        statement.startsWith('alter ') ||
-        statement.startsWith('drop ')
-    );
-  },
-  getCreateCheckConstraintSql: (
-    tableName: string,
-    schemaName: string,
-    constraintName: string,
-    check: string
-  ) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} add constraint \`${constraintName}\` check ${check};
-  `,
-  getCreatePkSql: ({
-    schemaName,
-    tableName,
-    selectedPkColumns,
-  }: CreatePKArgs) => `
-    alter table ${getMySQLNameString(
-      schemaName,
-      tableName
-    )} add primary key (${selectedPkColumns.join(', ')});
-  `,
+  getAlterForeignKeySql,
+  getCreateFKeySql,
+  getDropConstraintSql,
+  getRenameTableSql,
+  getDropTriggerSql,
+  getCreateTriggerSql,
+  getDropSql,
+  getDropColumnSql,
+  getAddColumnSql,
+  getAddUniqueConstraintSql,
+  getDropNotNullSql,
+  getSetCommentSql,
+  getSetColumnDefaultSql,
+  getSetNotNullSql,
+  getAlterColumnTypeSql,
+  getDropColumnDefaultSql,
+  getRenameColumnQuery,
+  checkSchemaModification,
+  getCreateCheckConstraintSql,
+  getCreatePkSql,
 };
--- console/src/dataSources/services/mysql/utils.ts
+++
@@ -1,9 +0,0 @@
-export const getMySQLNameString = (schemaName: string, itemName: string) =>
-  `\`${schemaName}\`.\`${itemName}\``;
-
-export const escapeText = (text: string | null) => {
-  if (!text) {
-    return 'NULL';
-  }
-  return `'%string "${text}" %'`;
-};
