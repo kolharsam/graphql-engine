922bd5e03501cce7e6baea8018da15c4199f74f7
Sameer Kolhar <kolhar730@gmail.com>
2020-09-08 14:41:01 +0530

    remove method

--- console/src/components/Services/Data/Function/customFunctionReducer.js
+++ console/src/components/Services/Data/Function/customFunctionReducer.js
@@ -158,71 +158,6 @@ const fetchCustomFunction = (functionName, schema, source) => {
   };
 };
 
-const deleteFunctionSql = () => {
-  return (dispatch, getState) => {
-    const currentSchema = getState().tables.currentSchema;
-    const source = getState().tables.currentDataSource;
-    const {
-      functionName,
-      functionDefinition,
-      inputArgTypes,
-    } = getState().functions;
-
-    const functionNameWithSchema =
-      '"' + currentSchema + '"' + '.' + '"' + functionName + '"';
-
-    let functionArgString = '';
-    if (inputArgTypes.length > 0) {
-      functionArgString += '(';
-      inputArgTypes.forEach((inputArg, i) => {
-        functionArgString += i > 0 ? ', ' : '';
-
-        functionArgString +=
-          '"' + inputArg.schema + '"' + '.' + '"' + inputArg.name + '"';
-      });
-      functionArgString += ')';
-    }
-
-    const sqlDropFunction =
-      'DROP FUNCTION ' + functionNameWithSchema + functionArgString;
-
-    const sqlUpQueries = [getRunSqlQuery(sqlDropFunction, source)];
-
-    const sqlDownQueries = [];
-    if (functionDefinition && functionDefinition.length > 0) {
-      sqlDownQueries.push(getRunSqlQuery(functionDefinition, source));
-    }
-
-    // Apply migrations
-    const migrationName = 'drop_function_' + currentSchema + '_' + functionName;
-
-    const requestMsg = 'Deleting function...';
-    const successMsg = 'Function deleted';
-    const errorMsg = 'Deleting function failed';
-
-    const customOnSuccess = () => {
-      dispatch(_push(getSchemaBaseRoute(currentSchema)));
-    };
-    const customOnError = () => {
-      dispatch({ type: DELETE_CUSTOM_FUNCTION_FAIL });
-    };
-
-    dispatch({ type: DELETING_CUSTOM_FUNCTION });
-    return dispatch(
-      makeRequest(
-        sqlUpQueries,
-        sqlDownQueries,
-        migrationName,
-        customOnSuccess,
-        customOnError,
-        requestMsg,
-        successMsg,
-        errorMsg
-      )
-    );
-  };
-};
-
 const unTrackCustomFunction = () => {
   return (dispatch, getState) => {
     const currentSchema = getState().tables.currentSchema;
@@ -452,8 +387,9 @@ const customFunctionReducer = (state = functionData, action) => {
 export {
   RESET,
   fetchCustomFunction,
-  deleteFunctionSql,
   unTrackCustomFunction,
   updateSessVar,
+  DELETE_CUSTOM_FUNCTION_FAIL,
+  DELETING_CUSTOM_FUNCTION,
 };
 export default customFunctionReducer;
