a34a88b0f1b8bc4afb82c95d8e84f58104e439a4
Sameer Kolhar <kolhar730@gmail.com>
2020-12-10 22:25:56 +0530

    add new helper to construct the connection string

--- console/src/components/Services/Data/Schema/ManageDBUtils.ts
+++ console/src/components/Services/Data/Schema/ManageDBUtils.ts
@@ -1,4 +1,5 @@
 import { DataSource } from '../../../../metadata/types';
+import { Driver } from '../../../../dataSources';
 
 export const getHostFromConnectionString = (datasource: DataSource) => {
   const connectionString =
@@ -14,4 +15,23 @@ export const getHostFromConnectionString = (datasource: DataSource) => {
   }
   // TODO: update this function with connection string for other databases
   return null;
+};
+
+export const makeConnectionStringFromConnectionParams = (
+  dbType: Driver,
+  host: string,
+  port: string,
+  username: string,
+  database: string,
+  password?: string
+) => {
+  if (dbType === 'postgres') {
+    if (!password) {
+      return `postgresql://${username}@${host}:${port}/${database}`;
+    }
+    return `postgresql://${username}:${password}@${host}:${port}/${database}`;
+  }
+
+  // TODO: update this function to work for the other database drivers
+  throw new Error('Not implemented for other database drivers');
 };
